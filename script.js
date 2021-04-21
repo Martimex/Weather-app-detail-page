1d2cbd6663c30daa3f25966edaeb00dc6836047e
Kamil Marciniak <76244675+Martimex@users.noreply.github.com>
2021-04-21 10:46:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const city = 'Kraków';
 
 let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=d10be5670d0e6307831a8eccb6cee0ef`;
 
-let limit = 10
+let limit = 10;
+
+let isFirstBlockDay; // is the first block representing day?
 
 // trzeba 10 razy fetch'ować dane
 
@@ -57,7 +59,11 @@ function last() {
                 let fullDate = data.list[(iter*4)+1].dt_txt;
 
                 // For first operation in the loop
-                (iter === 0)? dayOrNight(fullDate) : '';
+                dayOrNight(fullDate, iter);
+
+                (iter === 0)? separateDays(fullDate) : ''; // ta funkcja wykonana w pętli 10 razy, jest ASYNC !
+
+                const value = dayOrNight(fullDate);
 
                 let temp = Math.floor(data.list[(iter*4)+1].main.temp)+'°C';
                 let iconCode = data.list[(iter*4)+1].weather[0].icon;
@@ -67,7 +73,7 @@ function last() {
                 let num = Math.floor(data.list[(iter*4)+1].main.temp);
                 let baseNum = Math.floor(data.list[(0*4)+1].main.temp); // to bazowa temperatura dla itera-0
 
-                console.log(temp);
+                //console.log(temp);
     
                 document.querySelector(`.grid-container .date:nth-of-type(${iter+1})`).textContent = fullDate;
     
@@ -84,7 +90,7 @@ function last() {
                //console.log(icon);
                 //icon.setAttribute('src', iconurl);
                 
-                setBaseVisual(iter, visual, num, baseNum);
+                setBaseVisual(iter, visual, num, baseNum, value);
 
                 // Append img to upper div
 
@@ -102,14 +108,14 @@ function last() {
     let citySpan = document.querySelector('.city-name');
     citySpan.textContent = city;
 
-    separateDays();
+    //separateDays(fullDate);
 }
 
 first();
 last();
 
 
-function setBaseVisual(iter, visual, num, baseNum) {
+function setBaseVisual(iter, visual, num, baseNum, value) {
 
     // iter = która to iteracja
     // visual = bloczek grid, na którym wykonujemy operację
@@ -117,35 +123,61 @@ function setBaseVisual(iter, visual, num, baseNum) {
     // baseNum = temperatura bloczku 1, który zawsze wynosi 21 divów - pozostała ilość bloczków to już różnica temperatur
 
     if(iter == 0) {
-        visual.querySelectorAll(`div:nth-last-child(-n+21)`) // all divs, but we actually need 21
-        .forEach(elem =>  {console.log(elem); elem.style.background = `linear-gradient(135deg, #56cd, #6add)`}); 
+        (isFirstBlockDay)?
+            visual.querySelectorAll(`div:nth-last-child(-n+21)`) // all divs, but we actually need 21
+            .forEach(elem =>  {elem.style.background = `linear-gradient(135deg, #56cd, #6add)`}) // light
+        :
+            visual.querySelectorAll(`div:nth-last-child(-n+21)`) // all divs, but we actually need 21
+            .forEach(elem =>  {elem.style.background = `linear-gradient(135deg, #75dd, #349d)`}) // dark
     }
 
     else {
         //visual.querySelectorAll(`div`).forEach(div => {div.style.background = `linear-gradient(to bottom, rgba(255, 255, 255, 0.1))`})
+        (isFirstBlockDay)? 
 
-        (iter % 2)? 
-
-        visual.querySelectorAll(`div:nth-last-child(-n+${(num - baseNum) + 21})`)
-        .forEach(elem => {elem.style.background = `linear-gradient(135deg, #75dd, #349d)`})
+            (!(iter%2))?
+            visual.querySelectorAll(`div:nth-last-child(-n+${(num - baseNum) + 21})`)
+            .forEach(elem => {elem.style.background = `linear-gradient(135deg, #56cd, #6add)`})
+            :
+            visual.querySelectorAll(`div:nth-last-child(-n+${(num - baseNum) + 21})`) 
+            .forEach(elem => {elem.style.background = `linear-gradient(135deg, #75dd, #349d)`})   
 
         :
-
-        visual.querySelectorAll(`div:nth-last-child(-n+${(num - baseNum) + 21})`)
-        .forEach(elem => {elem.style.background = `linear-gradient(135deg, #56cd, #6add)`});
-        
+            (iter%2)?
+            visual.querySelectorAll(`div:nth-last-child(-n+${(num - baseNum) + 21})`) 
+            .forEach(elem => {elem.style.background = `linear-gradient(135deg, #75dd, #349d)`})
+            :
+            visual.querySelectorAll(`div:nth-last-child(-n+${(num - baseNum) + 21})`)
+            .forEach(elem => {elem.style.background = `linear-gradient(135deg, #56cd, #6add)`})   
     }
 }
 
-function separateDays() {
+function separateDays(firstDate) {
 
-    //const day = firstDate.substring(8,10);
-    //console.log(day);
+    const day = firstDate.substring(8,10);
+    const hour = parseInt(firstDate.substring(11,13));
+    console.log('Day:  ',day, '  Hour:  ',hour);
+
+    if(hour >= 12) {
+        console.log('it is');
+        document.querySelectorAll(`.grid-container .visual:nth-child(odd)`).forEach(visual => 
+            visual.style.cssText = `border-right: 0.32em solid #2222;`);
+    }
+
+    else {
+        console.log('nope');
+        document.querySelectorAll(`.grid-container .visual:nth-child(even)`).forEach(visual =>
+            visual.style = `border-right: 0.32em solid #2222;`);
+    }
 
 }
 
-function dayOrNight(firstDate) {
-    console.log(firstDate);
+function dayOrNight(firstDate, iter) {
+    //console.log(firstDate);
+
+    if(iter !== 0) {
+        return false;
+    }
 
     const dayValues = [9, 12, 15, 18];
     //const nightValues = [21, 0, 3, 6];
@@ -153,12 +185,18 @@ function dayOrNight(firstDate) {
     const Monthday = firstDate.substring(11,13);
     console.log(Monthday);
 
-    (dayValues.includes(parseInt(Monthday)))? // czy Monthday to g. 9, 12, 15, a może 18?
+    if(dayValues.includes(parseInt(Monthday)))  { // czy Monthday to g. 9, 12, 15, a może 18?
+        document.querySelectorAll(`.grid-container .date:nth-child(even)`).forEach(el => //tak
+        el.style.cssText = `background: #75d5; border-top: .3em solid #75d5; border-left: .3em solid #75d5; `);
+        isFirstBlockDay = true;
+        return true;
+    } 
 
-    document.querySelectorAll(`.grid-container .date:nth-child(even)`).forEach(el => //tak
-    el.style.cssText = `background: #75d5; border-top: .3em solid #75d5; border-left: .3em solid #75d5; `)
-    :
-    document.querySelectorAll(`.grid-container .date:nth-child(odd)`).forEach(el => //nie
-    el.style.cssText = `background: #75d5; border-top: .3em solid #75d5; border-left: .3em solid #75d5;`)
+    else {
+        document.querySelectorAll(`.grid-container .date:nth-child(odd)`).forEach(el => //nie
+        el.style.cssText = `background: #75d5; border-top: .3em solid #75d5; border-left: .3em solid #75d5;`);
+        isFirstBlockDay = false;
+        return false;
+    } 
         
 }
